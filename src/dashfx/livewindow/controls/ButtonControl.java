aba44ac2883697f81249efa776c86ab1920aeae8
Patrick Plenefisch <simonpatp@gmail.com>
2013-10-27 14:47:35 -0400

    Fixing dependence on old Label property

--- src/dashfx/livewindow/controls/ButtonControl.java
+++ src/dashfx/livewindow/controls/ButtonControl.java
@@ -5,22 +5,42 @@
 package dashfx.livewindow.controls;
 
 import dashfx.controls.bases.BooleanControlBase;
+import dashfx.lib.controls.Category;
+import dashfx.lib.controls.Designable;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.ToggleButton;
 
 /**
  *
  * @author Sam
  */
-public class ButtonControl extends BooleanControlBase {
-
+public class ButtonControl extends BooleanControlBase
+{
 	private final ToggleButton button = new ToggleButton();
 
-	public ButtonControl() {
+	public ButtonControl()
+	{
 		super();
-		button.textProperty().bind(labelProperty());
 		button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		button.selectedProperty().bindBidirectional(valueProperty());
 		setUi(button);
 	}
-	
+
+	// nice wrapper
+	@Designable(value = "Text", description = "What to label the button")
+	@Category("Basic")
+	public StringProperty textProperty()
+	{
+		return button.textProperty();
+	}
+
+	public String getText()
+	{
+		return textProperty().get();
+	}
+
+	public void setText(String text)
+	{
+		textProperty().set(text);
+	}
 }
